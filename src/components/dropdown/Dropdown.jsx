50fb6a52a15446e8196e47c391f8ee14cfa24980
Kaio Lamounier <82100451+kaioLamounier12@users.noreply.github.com>
2022-08-28 21:10:42 -0300

    Dropdown Settings and styles

--- src/components/dropdown/Dropdown.jsx
+++ src/components/dropdown/Dropdown.jsx
@@ -1,11 +1,33 @@
+import { useState } from "react"
 import "./dropdown.css"
 
-const Dropdown = () => {
+const Dropdown = (props) => {
+
+  const [show, setShow] = useState(false)
+
+  const toggle = () => {show? setShow(false) : setShow(true)}
+
+  const listMenu = props.menuItems.map((obj) => {
+    return (
+      <li key={obj.text} onClick={obj.onClick}>{obj.text}</li>
+    )
+  })
+
   return (
-    <>
-    
-    </>
+    <button className={`actionDrop align-${props.alignment} ${props.className}`} onClick={toggle}>
+        {props.children}
+        <div className={["menuDrop", show? "active" : ""].join(" ")}>
+            <ul>
+             {listMenu}
+            </ul>
+        </div>
+    </button>
   )
+}
+
+Dropdown.defaultProps = {
+  className: "",
+  alignment: "left"
 }
 
 export default Dropdown
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,14 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import Dropdown from "../dropdown/Dropdown"
 import "./header.css"
 
 const Header = () => {
+
+    const navigate = useNavigate()
+
+    const menuItems = [
+        {text: "Alert foo", onClick: () => {alert("foo")}},
+        {text: "Home", onClick: () => {navigate("/Home")}}]
 
     return (
         <>
@@ -11,7 +17,7 @@ const Header = () => {
                     <div className="header-content">
                         <h1 className="logo"><Link to="/">LOGO</Link></h1>
                         <nav className="nav">
-                            <li><Dropdown></Dropdown></li>
+                            <li><Dropdown menuItems={menuItems} alignment="left">Dropdown</Dropdown></li>
                         </nav>
                         <div className="header-search">
                             
