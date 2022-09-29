9d423af62daa5cef214b6114b99ac81847fe836c
Kaio  Lamounier <kaiodejesus223@gmail.com>
2022-09-29 09:20:37 -0300

    Update firebase.jsx

--- src/firebase/firebase.jsx
+++ src/firebase/firebase.jsx
@@ -3,12 +3,12 @@ import { getFirestore, addDoc, getDocs, collection } from "firebase/firestore";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword  } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA_48T30wZY7ll8YxAlUlpDToN8Grr3JSA",
-  authDomain: "project-teste-9ee70.firebaseapp.com",
-  projectId: "project-teste-9ee70",
-  storageBucket: "project-teste-9ee70.appspot.com",
-  messagingSenderId: "2812345805",
-  appId: "1:2812345805:web:0f436882cac576be5ad884"
+  apiKey: "AIzaSyAFtz7F5zOUcGW_kNCt-T3uj_VMiFBSmnY",
+  authDomain: "projeto-tester-d7289.firebaseapp.com",
+  projectId: "projeto-tester-d7289",
+  storageBucket: "projeto-tester-d7289.appspot.com",
+  messagingSenderId: "630745349893",
+  appId: "1:630745349893:web:11a5eedd00576dde10416c"
 };
 
 const app = initializeApp(firebaseConfig);
